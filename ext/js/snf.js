5fbf55b0258d0f37a57b78953337b80a2febe2e0
ben fausch <benjamin.fausch@gmail.com>
2017-11-25 14:21:23 -0700

    cleanup of containers

--- ext/js/snf.js
+++ ext/js/snf.js
@@ -1,9 +1,5 @@
 //TODO:
-//handle empty returns from all api queries
-//api key
-// console.log('test')
-
-// theMovieDb.common.api_key = "117df4e7395c65b9ea3b70a9a3f5a3f5";
+//handle empty returns from all api queries, handle tv shows
 //search for movies by title
 function movieSearch(query, that) {
     let title = encodeURIComponent(query);
@@ -15,6 +11,7 @@ function movieSearch(query, that) {
         data = JSON.parse(data)
         let id = data.results[0].id;
         console.log('id is ' + id)
+        closeModal()
         getMovieTrailer(id)
         getMovieById(id, that)
         // getOMDB(id)
@@ -41,8 +38,10 @@ function getMovieTrailer(movieId) {
         data = JSON.parse(data)
         let youtube = data.youtube[0].source
         console.log(youtube)
-
-        $('.jawBoneContent.open .jawbone-overview-info .listMeta').after('<p class="snf-trailer" id="' + youtube + '"><a href="https://www.youtube.com/watch?v='+youtube+'" target="_blank">Official Trailer</a></button>')
+        if ($('.snf-trailer').length > 0) {
+            $('.snf-trailer').remove()
+        }
+        $('.jawBoneContent.open .jawbone-overview-info .listMeta').after('<p class="snf-trailer" id="' + youtube + '"><a href="https://www.youtube.com/watch?v=' + youtube + '" target="_blank">Official Trailer</a></button>')
     }, function(error) {
         console.log(error)
     })
@@ -63,41 +62,40 @@ function getOMDB(imdb, title, date, that) {
         console.log(data)
         console.log('current selector')
         console.log(that)
-
+        if ($('.snf-ratings').length > 0) {
+            $('.snf-ratings').remove()
+        }
         data.Ratings.forEach(function(rating) {
             $('.jawBoneContent.open .jawbone-overview-info .listMeta').unbind().after('<p class="snf-ratings">' + rating.Source + ' : <span>' + rating.Value + '</span></p>')
         })
     })
 }
 
-
-function closeModal(){
+function closeModal() {
     console.log('close')
-            $('#snf-container').empty()
-            $('.snf-ratings').remove()
-            $('.snf-trailer').remove()
+    $('.snf-ratings').remove()
+    $('.snf-trailer').remove()
 }
-
 $(document).ready(function() {
     //on hover over title card
     $('.slider-item').unbind().on('mouseenter', function() {
         console.log('title card')
         let title = $(this).find('.video-preload-title-label').text().trim();
-        closeModal()
-        $(this).next('.jawBoneContent.open').css('border','3px sold green')
-        
-        let movieId = movieSearch(title, this)
-        // $(this).find('.title-card span').unbind().on('click', function() {
-        //     closeModal()
-        //     console.log('clicked ' + title)
-        //     let movieId = movieSearch(title)
-        // });
-        $('.close-button').unbind().on('click', function() {
-            closeModal()
+        setTimeout(function() {
+            movieSearch(title, this);
+        }, 1500);
+        $('.title-card span').unbind().on('click', function() {
+            console.log('clicked ' + title)
+            setTimeout(function() {
+                movieSearch(title, this);
+            }, 1500);
         });
-    })
-        // $('.slider-item').unbind().on('mouseleave', function() {
+        // $('.close-button').unbind().on('click', function() {
         //     closeModal()
-        // })
+        // });
+    });
+    // $('.slider-item').unbind().on('mouseleave', function() {
+    //     closeModal()
+    // })
     //end doc ready
 });
